22bb1a9a3069231faa9e40b16270bb8469de305d
Thibaut Robert <thibaut.robert@worldline.com>
2015-11-19 13:11:38 +0100

    validator without score

--- src/main/scala/hashcode/Validator.scala
+++ src/main/scala/hashcode/Validator.scala
@@ -9,13 +9,13 @@ object Validator {
       assert(slice.p1.row <= slice.p2.row, s"Incorrect slice: $slice")
       assert(slice.p1.col <= slice.p2.col, s"Incorrect slice: $slice")
       assert( (slice.p2.row - slice.p1.row +1) * (slice.p2.col - slice.p2.col) <= problem.maxCells, s"slice $slice is too big")
-      val pSlice = problem.pizza.slice(slice.p1.row, slice.p2.row) map { _.slice(slice.p1.col, slice.p2.col) }
+      val pSlice = problem.pizza.slice(slice.p1.row, slice.p2.row+1) map { _.slice(slice.p1.col, slice.p2.col+1) }
       val nHam = pSlice.flatten.count('H'.==)
-      assert(nHam < problem.nHam, s"slice $slice contains $nHam, expected ${problem.nHam}")
+      assert(nHam >= problem.nHam, s"slice $slice contains $nHam, expected ${problem.nHam}")
     }
 
     Try {
-      solution.sol foreach  validateSlice
+      solution.sol foreach validateSlice
       42
     }
 
