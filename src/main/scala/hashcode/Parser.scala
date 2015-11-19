a1000f4b39f1ed242d0ee81f3894c930ccd66523
Thibaut Robert <thibaut.robert@worldline.com>
2015-11-19 12:34:00 +0100

    parsing

--- src/main/scala/hashcode/Parser.scala
+++ src/main/scala/hashcode/Parser.scala
@@ -2,15 +2,10 @@ package hashcode
 
 object Parser {
   def read(): Problem = {
-    val input = io.Source.fromFile("input/data.in").getLines.toList
+    val input = io.Source.fromFile("input/data.in.txt").getLines().toList
     val first = input.head
-    val Array(nbRows, nbSlots, nbUnavailable, nbPools, nbServers) = first.split(" ").map(_.toInt)
+    val Array(nbRows, nbCols, h, s) = first.split(" ").map(_.toInt)
 
-    val servers = for {
-      (line, i) <- input.tail.drop(nbUnavailable).take(nbServers).zipWithIndex
-      Array(s, c) = line.split(" ")
-    } yield Server(s.toInt, c.toInt, i)
-
-    Problem(servers)
+    Problem(nbRows, nbCols, h, s, input.tail)
   }
 }
--- src/main/scala/hashcode/Problem.scala
+++ src/main/scala/hashcode/Problem.scala
@@ -4,5 +4,5 @@ case class Server(size: Int, capacity: Int, id: Int) {
   def ratio: Float = capacity / size.toFloat
 }
 
-case class Problem(servers: List[Server])
+case class Problem(nbRows: Int, nbCols: Int, nHam: Int, maxCells:Int , pizza: List[String])
 
