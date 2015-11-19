ec339fe29ab1f5e7f8204c7b88e8c117921ad1d7
Thibaut Robert <thibaut.robert@worldline.com>
2015-11-19 13:47:08 +0100

    slice size and overlapping

--- src/main/scala/hashcode/Solution.scala
+++ src/main/scala/hashcode/Solution.scala
@@ -5,5 +5,7 @@ case class Point(row: Int, col:Int) {
 }
 case class Slice(p1: Point, p2: Point) {
   override def toString = s"$p1 $p2"
+  def overlaps(o:Slice) = !(o.p2.row < p1.row || p2.row < o.p1.row || o.p2.col < p1.col || p2.col < o.p1.col)
+  def size = (p2.row - p1.row + 1) * (p2.col - p1.col + 1)
 }
 case class Solution(sol: List[Slice])
--- src/main/scala/hashcode/Validator.scala
+++ src/main/scala/hashcode/Validator.scala
@@ -10,7 +10,7 @@ object Validator {
       assert(slice.p1.col <= slice.p2.col, s"Incorrect slice: $slice")
       assert(slice.p1.row >= 0 && slice.p2.row < problem.nbRows, s"Incorrect slice: $slice")
       assert(slice.p1.col >= 0 && slice.p2.col < problem.nbCols, s"Incorrect slice: $slice")
-      assert( sliceSize(slice) <= problem.maxCells, s"slice $slice is too big")
+      assert( slice.size <= problem.maxCells, s"slice $slice is too big")
       val pSlice = problem.pizza.slice(slice.p1.row, slice.p2.row+1) map { _.slice(slice.p1.col, slice.p2.col+1) }
       val nHam = pSlice.flatten.count('H'.==)
       assert(nHam >= problem.nHam, s"slice $slice contains $nHam, expected ${problem.nHam}")
@@ -18,17 +18,11 @@ object Validator {
 
     Try {
       solution.sol foreach validateSlice
-      solution.sol.combinations(2) foreach { case List(s1,s2) ⇒ checkNonOverlap(s1,s2) }
-      (solution.sol map sliceSize).sum
+      solution.sol.combinations(2) foreach { case List(s1,s2) ⇒
+        assert(!s1.overlaps(s2), s"Slice $s1 and slice $s2 overlaps")
+      }
+      (solution.sol map (_.size)).sum
     }
 
-  }
-
-  def sliceSize(slice: Slice): Int = {
-    (slice.p2.row - slice.p1.row + 1) * (slice.p2.col - slice.p1.col + 1)
-  }
-  def checkNonOverlap(slice1: Slice, slice2:Slice) = {
-    assert(slice2.p2.row < slice1.p1.row || slice1.p2.row < slice2.p1.row || slice2.p2.col < slice1.p1.col || slice1.p2.col < slice2.p1.col,
-    s"Slice $slice1 and slice $slice2 overlaps")
   }
 }
