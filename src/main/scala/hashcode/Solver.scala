32437a1e2d15df06869e587494d23c142cce910c
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-11-19 12:57:00 +0100

    naive solution

--- src/main/scala/hashcode/Solver.scala
+++ src/main/scala/hashcode/Solver.scala
@@ -1,6 +1,40 @@
 package hashcode
 
 object Solver {
-  def solve(problem: Problem): Solution =
-    Solution(42)
+
+  case class Orientation(rows: Int = 12, cols: Int = 1)
+  case class Pizza(topleft: Point, botRight: Point, ham: Map[Point, Boolean]) {
+    def asSlice = Slice(topleft, botRight)
+
+    def isOk(problem: Problem) = {
+      ham.values.count(identity) >= problem.nHam
+    }
+  }
+
+  def solve(problem: Problem): Solution = {
+    val slices = for {
+      pizza <- split(problem, Orientation())
+      if pizza.isOk(problem)
+    } yield pizza.asSlice
+
+    Solution(slices.toList)
+  }
+
+  def split(problem: Problem, orientation: Orientation): List[Pizza] = {
+    val pizzas = for {
+      row <- 0 until problem.nbRows by orientation.rows
+      col <- 0 until problem.nbCols by orientation.cols
+    } yield {
+      val rowEnd = orientation.rows + row - 1
+      val colEnd = orientation.cols + col - 1
+      val hams = (for {
+        i <- row to rowEnd
+        j <- col to colEnd
+        ham = problem.pizza(row)(col) == 'H'
+      } yield Point(i, j) -> ham).toMap
+
+      Pizza(Point(row, col), Point(rowEnd, colEnd), hams)
+    }
+    pizzas.toList
+  }
 }
