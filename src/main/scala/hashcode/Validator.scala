a30e98aaf9e02273b907caad12659ef07c387419
Thibaut Robert <thibaut.robert@worldline.com>
2015-11-19 13:01:15 +0100

    validator without score

--- src/main/scala/hashcode/Validator.scala
+++ src/main/scala/hashcode/Validator.scala
@@ -1,8 +1,24 @@
 package hashcode
 
-import scala.util.{Success, Try}
+import scala.util.Try
 
 object Validator {
-  def score(solution: Solution, problem: Problem): Try[Int] =
-    Success(42)
+
+  def score(solution: Solution, problem: Problem): Try[Int] = {
+    def validateSlice(slice: Slice) = {
+      assert(slice.p1.row <= slice.p2.row, s"Incorrect slice: $slice")
+      assert(slice.p1.col <= slice.p2.col, s"Incorrect slice: $slice")
+      assert( (slice.p2.row - slice.p1.row +1) * (slice.p2.col - slice.p2.col) <= problem.maxCells, s"slice $slice is too big")
+      val pSlice = problem.pizza.slice(slice.p1.row, slice.p2.row) map { _.slice(slice.p1.col, slice.p2.col) }
+      val nHam = pSlice.flatten.count('H'.==)
+      assert(nHam < problem.nHam, s"slice $slice contains $nHam, expected ${problem.nHam}")
+    }
+
+    Try {
+      solution.sol foreach  validateSlice
+      42
+    }
+
+  }
+
 }
