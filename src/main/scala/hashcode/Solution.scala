e4310d4c1ce1d82a36af0cab3a0c347177b9d231
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-11-19 12:56:53 +0100

    fix format

--- src/main/scala/hashcode/Solution.scala
+++ src/main/scala/hashcode/Solution.scala
@@ -4,6 +4,6 @@ case class Point(row: Int, col:Int) {
   override def toString = s"$row $col"
 }
 case class Slice(p1: Point, p2: Point) {
-  override def toString = s"$p1 $p2"
+  override def toString = s"${p1.row} ${p2.row} ${p1.col} ${p2.col}"
 }
 case class Solution(sol: List[Slice])
