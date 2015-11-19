d9f38b4f38c9b9795504544495049dc81e3a6c2b
Thibaut Robert <thibaut.robert@worldline.com>
2015-11-19 12:45:01 +0100

    solution

--- src/main/scala/hashcode/Formatter.scala
+++ src/main/scala/hashcode/Formatter.scala
@@ -6,7 +6,8 @@ object Formatter {
   def write(solution: Solution, score: Int): Unit = {
     val name = s"out.${score}.txt"
     val f = new PrintStream(name)
-    f.println(solution.toString)
+    f.println(solution.sol.length)
+    f.println(solution.sol.mkString("\n"))
     f.close
     println(s"wrote to $name")
   }
--- src/main/scala/hashcode/Solution.scala
+++ src/main/scala/hashcode/Solution.scala
@@ -1,5 +1,9 @@
 package hashcode
 
-case class Point(row: Int, col:Int)
-case class Slice(p1: Point, p2: Point)
+case class Point(row: Int, col:Int) {
+  override def toString = s"$row $col"
+}
+case class Slice(p1: Point, p2: Point) {
+  override def toString = s"$p1 $p2"
+}
 case class Solution(sol: List[Slice])
