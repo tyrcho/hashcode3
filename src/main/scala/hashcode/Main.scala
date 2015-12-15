a84b7e4787ecf5db27a50d12acd25808cd2f929b
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-12-15 14:31:41 +0100

    new implementation (WIP : very slow)

--- src/main/scala/hashcode/Main.scala
+++ src/main/scala/hashcode/Main.scala
@@ -4,7 +4,7 @@ import scala.util.{ Failure, Success }
 
 object Main extends App {
   val problem = Parser.read()
-  val solution = Solver.solve3(problem)
+  val solution = NewSolver.solve(problem)
   display(problem, solution)
   Validator.score(solution, problem) match {
     case Success(score) =>
@@ -26,40 +26,40 @@ object Main extends App {
         case _ => sliceColors
       }
     }
-    
-    def constructPointMap(slices: List[Slice], sliceColors: Map[Slice, String], pointColors: Map[Point,String]) : Map[Point,String] = {
+
+    def constructPointMap(slices: List[Slice], sliceColors: Map[Slice, String], pointColors: Map[Point, String]): Map[Point, String] = {
       slices match {
         case head :: tail =>
           val sliceColor = sliceColors.get(head).get
           val coloredPoints = for {
             row <- head.p1.row to head.p2.row
             col <- head.p1.col to head.p2.col
-          }yield{
-            (Point(row,col),sliceColor)
+          } yield {
+            (Point(row, col), sliceColor)
           }
           constructPointMap(tail, sliceColors, pointColors ++ coloredPoints.toMap)
         case _ => pointColors
       }
     }
-    
-    def colorProblem(problem: Problem, pointColors: Map[Point, String]) : List[String] = {
+
+    def colorProblem(problem: Problem, pointColors: Map[Point, String]): List[String] = {
       val colorLines = for {
         row <- 0 until problem.nbRows
-      }yield{
+      } yield {
         val line = problem.pizza(row)
         val colorCharacters = for {
           col <- 0 until problem.nbCols
-        }yield{
-          pointColors.getOrElse(Point(row,col), Console.WHITE) + problem.pizza(row)(col) + Console.RESET
+        } yield {
+          pointColors.getOrElse(Point(row, col), Console.WHITE) + problem.pizza(row)(col) + Console.RESET
         }
-        colorCharacters.toList.reduce(_+_)
+        colorCharacters.toList.reduce(_ + _)
       }
       colorLines.toList
     }
 
     val sliceColors = constructMap(solution.sol, Map.empty)
-    val pointColors= constructPointMap(solution.sol, sliceColors, Map.empty)
-    val problemColor = colorProblem(problem,pointColors)
+    val pointColors = constructPointMap(solution.sol, sliceColors, Map.empty)
+    val problemColor = colorProblem(problem, pointColors)
     println(problemColor.mkString("\n").replace('T', ' '))
   }
 }
